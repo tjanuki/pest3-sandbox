d322be6b7601caf4be434dd1af03814839b4c9e2
tjanuki <t.januki@gmail.com>
2024-10-04 15:12:04 -0400

    Add laravel arch tests

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -3,5 +3,19 @@
 
 arch()->expect('dd')->not->toBeUsed();
 
-arch()->expect('App\Http')
-    ->toOnlyBeUsedIn('App\Http');
+//arch()->expect('App\Http')
+//    ->toOnlyBeUsedIn('App\Http');
+
+arch()
+    ->expect('App')
+    ->not->toUse(['die', 'dd', 'dump']);
+
+arch()->preset()->laravel();
+
+//arch('app')
+//    ->expect('App\Models')
+//    ->toBeAbstract();
+
+arch('app')
+    ->expect('App\Enums')
+    ->toBeEnums();
